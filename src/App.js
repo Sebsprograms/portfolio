1272434b6ff39571b70ee4ee94df097d1a93cc09
Sebsprograms <sebastianbenad72@gmail.com>
2023-07-15 16:40:29 -0600

    Create Footerbar component styled similar to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import FooterBar from './components/navigation/FooterBar';
 import NavBar from './components/navigation/NavBar';
 
 function App() {
   return (
     <div className="App primary-bg-color">
       <NavBar />
+      <div id='main-body'>
+        {/* Future content will go here */}
+      </div>
+      <FooterBar />
     </div>
   );
 }
---
+++ src/components/navigation/SocialIcon.js
@@ -0,0 +1,11 @@
+
+function SocialIcon(props) {
+    return (
+        <a href={props.link} className={"light-border text-off-color " + (props.githubName ? 'github-icon' : 'social-icon')} target="_blank">
+            <i className={`fa-brands ${props.iconClass}`}></i>
+            {props.githubName && <p className="icon-title">{props.githubName}</p>}
+        </a>
+    );
+}
+
+export default SocialIcon;
